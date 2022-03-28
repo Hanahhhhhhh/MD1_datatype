3f6f360c86e171fcfb7cccceccf111936ef0cba1
hanahhhhhhhh <hanguyen18052004@gmail.com>
2022-03-28 08:26:28 +0700

    messeage

---
+++ java.js
@@ -0,0 +1,10 @@
+<script>
+    physics =prompt("nhap diem physics");
+    chemistry =prompt("nhap diem chemistry");
+    biology =prompt("nhap diem biology");
+    let average= (physics + chemistry + biology)/3;
+    document.write('Average'+average)
+    document.write('<br/>')
+    let sum= (physics + chemistry + biology);
+    document.write('Sum'+sum)
+</script>
